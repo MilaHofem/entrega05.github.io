534d75b13dda50128d8bdb144ed30bafba241208
MilaHofem <110565465+MilaHofem@users.noreply.github.com>
2022-10-07 14:16:37 -0300

    css y js

---
+++ js/cart.js
@@ -0,0 +1,60 @@
+let carritoArray = [];
+
+let precio = parseInt(document.getElementById("costoProd"));
+let cantidad = parseInt(document.getElementById("cantProd"));
+let subtotal = 0;
+
+
+function subtotal(precio){
+	
+	subtotal = precio*cantidad;
+	return subtotal;
+    console.log(cantidad)
+}
+
+
+function mostrarDatosCarrito(array){
+
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let item = array[i];
+        htmlContentToAppend += `
+        
+        </div>
+        <div class="col">
+        <img src="`+ item.image +`" id="nombreProd" alt="imagenItem"></p>
+        </div>
+        <div class="col">
+          <p id="nombreProd">`+ item.name +`</p>
+        </div>
+        <div class="col">
+          <p id="costoProd">`+ item.currency +` `+ item.unitCost +`</p>
+        </div>
+          <div class="col">
+            <input class="form-control" type="number" onchange="calculo(${item.unitCost}") id="cantProd" value=`+ item.count +`> 
+          </div>
+          <div class="col">
+            <p id="subProd"> `+(precio*cantidad)+` </p>
+          </div>
+
+        </div>
+         
+        
+        `
+        document.getElementById("carrito-list-container").innerHTML = htmlContentToAppend;
+        /* document.getElementById("subProd").innerHTML += subtotal; */
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    getJSONData(carrito).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            carritoArray = resultObj.data.articles;
+            mostrarDatosCarrito(carritoArray);
+        }
+
+    });
+ });
